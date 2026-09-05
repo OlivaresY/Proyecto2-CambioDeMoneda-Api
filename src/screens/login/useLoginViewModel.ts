87a531448ac254df76977b7a8b1691b20856f341
OlivaresY <124736762+OlivaresY@users.noreply.github.com>
2026-09-05 02:02:01 -0600

    feat(login): update useLoginViewModel hook to match AuthContext user structure

---
+++ src/screens/login/useLoginViewModel.ts
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { useAuth } from "../../contexts/AuthContext";
+
+export const useLoginViewModel = () => {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+
+    //coneccion con autentificacion
+    const { login } = useAuth();
+
+    const handleLogin = async () => {
+        if (!email || !password) {
+            setError('Please enter both email and password.');
+            return;
+        }
+
+        setLoading(true);
+        setError(null);
+        try {
+            login({ 
+                id: '1', 
+                name: email,
+                role: 'CLIENT' //o ADMIN
+            });
+        } catch (err) {
+            setError('Failed to login. Please check your credentials.');
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return {
+        email,
+        setEmail,
+        password,
+        setPassword,
+        loading,
+        error,
+        handleLogin,
+    };
+};
