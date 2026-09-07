ab109114999d1e85c2173723f4a12ba08afe221a
OlivaresY <124736762+OlivaresY@users.noreply.github.com>
2026-09-07 01:37:53 -0600

    feat: configure tabs layout with dynamic theme styling and icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,9 @@ import React, { useEffect } from "react";
 import { AuthProvider, useAuth } from "../src/contexts/AuthContext";
 import { ThemeProvider } from "../src/contexts/ThemeContext";
 
-//componente para manejar la logpica de redireccion en la session
-function RootNavihation() {
-  const { user, isAuthenticated } = useAuth();
+//componente para manejar la lógica de redirección en la sesión
+function RootNavigation() {
+  const { isAuthenticated } = useAuth();
   const segments = useSegments();
   const router = useRouter();
 
@@ -13,13 +13,13 @@ function RootNavihation() {
     const inTabsGroup = segments[0] === "(tabs)";
 
     if (!isAuthenticated && inTabsGroup) {
-      //si no esta autentificado y trata de entrar alos tabs, se redirige al login
+      //si no está autentificado y trata de entrar a los tabs, se redirige al login
       router.replace('/');
     } else if (isAuthenticated && !inTabsGroup) {
-      //si esta autentificado y esta en login, se redirige a la calculadora
+      //si está autentificado y está en login, se redirige a la calculadora
       router.replace('/(tabs)/calculator');
     }
-  }, [isAuthenticated, segments]);
+  }, [isAuthenticated, segments, router]);
   
   return <Slot />;
 }
@@ -28,7 +28,7 @@ export default function RootLayout() {
   return (
     <AuthProvider>
       <ThemeProvider>
-        <RootNavihation />
+        <RootNavigation />
       </ThemeProvider>
     </AuthProvider>
   );
